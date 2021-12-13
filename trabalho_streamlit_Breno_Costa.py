b4a97ec0cdbc2de83c2da2e8755bfd2bbe77c119
Breno Lomasso <brenomlomasso@gmail.com>
2021-12-13 07:17:53 -0300

    WIP

---
+++ trabalho_streamlit_Breno_Costa.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+
+survey = pd.read_csv("dados/survey_results_public.csv")
+schema = pd.read_csv("dados/survey_results_schema.csv")
+
+"""
+# Python para Ciência de Dados
+## Trabalho de streamlit\n
+Breno Marques Lomasso Costa
+"""
+
+"#### 1- Porcentagem das pessoas que responderam que se consideram profissionais, não profissionais, estudante, hobby, ...."
+
+"""---"""
+
+"#### 2- Distribuição das pessoas que responderam por localidade. Qual o país que teve maior participação?"
+
+"""---"""
+
+"#### 3- Qual a distribuição nível de estudo dos participantes?"
+
+"""---"""
+
+"#### 4- Qual a distribução de tempo de trabalho para cada tipo de profissional respondido na questão 1."
+
+"""---"""
+
+"#### 5- Das pessoas que trabalham profissionalmente:"
+"##### 5.1- qual a profissão delas?"
+"##### 5.2- qual a escolaridade?"
+"##### 5.3- qual o tamanho das empresas de pessoas que trabalham profissionalmente."
+
+"""---"""
+
+"#### 6- Média salarial das pessoas que responderam."
+
+"""---"""
+
+"#### 7- Pegando os 5 países que mais responderam o questionário, qual é o salário destas pessoas?"
+
+"""---"""
+
+"#### 8- Qual a porcentagem das pessoas que trabalham com python?"
+
+"""---"""
+
+"#### 9- Sobre python:"
+"##### 9.1- Qual o nível de salário de quem trabalha com python globalmente?"
+"##### 9.2- Para o Brasil, qual o nível salarial?"
+
+"""---"""
+
+"#### 10- Para os 5 países que mais tiveram participação, qual a média salarial?"
+
+"""---"""
+
+"#### 11- De todos as pessoas, Qual o sistema operacional utilizado por elas?"
+
+"""---"""
+
+"#### 12- Das pessoas que trabalham com python, qual a distribuição de sistema operacional utilizado por elas."
+
+"""---"""
+
+"#### 13- Qual a média de idade das pessoas que responderam?"
+
+"""---"""
+
+"#### 14- E em python? Qual a média de idade?"
